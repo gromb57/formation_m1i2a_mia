62ddc6d4eee3620ab8b719bf56711b5a002c2ee4
Bach Jérôme <jeromeb57@gmail.com>
2022-12-04 22:53:32 +0100

    update devoir_2

--- devoir_2/devoir_2_ex1.pl
+++ devoir_2/devoir_2_ex1.pl
@@ -23,7 +23,7 @@ solution(S):-
     bung(centre,H3,N3,F3), bung(limousin,H4,N4,F4), bung(picardie,H5,N5,F5)],
     % 1.2. Les horaires d’arrivée sont : 9h30, 11h, 13h, 15h, 16h30.
     elementMembre([H1,H2,H3,H4,H5], [1,2,3,4,5]),
-    % 1.3. Les nombres de personnes sont : 9, 12, 17, 20, 24.
+    % 1.3. Les numéros de bungalows sont : 9, 12, 17, 20, 24.
     elementMembre([N1,N2,N3,N4,N5], [9,12,17,20,24]),
     % 1.4. Les familles sont : 2, 3, 4, 5, 6.
     elementMembre([F1,F2,F3,F4,F5], [2,3,4,5,6]),
@@ -57,7 +57,7 @@ solution2(S):-
     H2 < H5,
     % 1.13. Les Limousins sont arrivés à 11h.
     H4 = 2,
-    % 1.3. Les nombres de personnes sont : 9, 12, 17, 20, 24.
+    % 1.3. Les numéro sont : 9, 12, 17, 20, 24.
     elementMembre([N1,N2,N3,N4,N5], [9,12,17,20,24]),
     % 1.7. Les Picards sont logés au numéro 9.
     N5 = 9,
@@ -79,42 +79,42 @@ solution2(S):-
 % 3. PLC
 :- use_module(library(clpfd)).
 
-solution3([[alsace,bourgogne,centre,limousin,picardie],
+solution3([[Alsace,Bourgogne,Centre,Limousin,Picardie],
     [H1,H2,H3,H4,H5],
     [N1,N2,N3,N4,N5],
-    [F1,F2,F3,F4,F5]]):-
+    [F1,F2,F3,F4,F5]], Type):-
     % 1.1.
-    [alsace,bourgogne,centre,limousin,picardie] ins 1..5,
-    all_different([alsace,bourgogne,centre,limousin,picardie]),
+    [Alsace,Bourgogne,Centre,Limousin,Picardie] ins 1..5,
+    all_different([Alsace,Bourgogne,Centre,Limousin,Picardie]),
     % 1.2. Les horaires d’arrivée sont : 9h30, 11h, 13h, 15h, 16h30.
     [H1,H2,H3,H4,H5] ins 1..5,
     all_different([H1,H2,H3,H4,H5]),
-    % 1.3. Les nombres de personnes sont : 9, 12, 17, 20, 24.
+    % 1.3. Les numéros de bungalows sont sont : 9, 12, 17, 20, 24.
     [N1,N2,N3,N4,N5] ins 1..5,
     all_different([N1,N2,N3,N4,N5]),
     % 1.4. Les familles sont : 2, 3, 4, 5, 6.
     [F1,F2,F3,F4,F5] ins 2..6,
     all_different([F1,F2,F3,F4,F5]),
     % 1.5. Les Bourguignons sont arrivés deux heures après la famille composée de 4 membres.
-    member(H2, [3,4]), F4 #\= 4,
+    (Bourgogne #= H3 #\/ Bourgogne #= H4) #/\ Bourgogne #\= F3,
     % 1.6. Les Bourguignons sont arrivés avant les Picards.
-    H2 #< H5,
+    (Picardie #= H4 #\/ Picardie #= H5) #/\ Bourgogne #< Picardie,
     % 1.7. Les Picards sont logés au numéro 9.
-    N5 #= 9,
+    Picardie #= N1,
     % 1.8. Les Picards ne sont pas trois.
-    F5 #\= 3,
+    Picardie #\= F2,
     % 1.9. Deux personnes occupent le bungalow numéro 12 et elles ne sont pas Alsaciennes ni Bourguignones.
-    member(bung(R9,_,12,2), S), R9 #\= alsace, R9 #\= bourgogne,
+    F1 #= N2, F1 #\= Alsace, F1 #\= Bourgogne,
     % 1.10. La famille composée de 5 membres est logée au numéro 24. Elle est arrivée juste avant les Alsaciens.
-    member(bung(_,H10,24,5), S), H10 #< H1,
+    F4 #= N5, F4 #< Alsace,
     % 1.11. Les Alsaciens ne sont pas quatre.
-    F1 #\= 4,
+    Alsace #\= F3,
     % 1.12. Les Limousins occupent le bungalow numéro 17
-    N4 #= 17,
+    Limousin #= N3,
     % 1.13. Les Limousins sont arrivés à 11h.
-    H4 #= 2.
+    Limousin #= H2,
     % Enumeration des solutions
-    labeling([], [alsace,bourgogne,centre,limousin,picardie,
+    labeling(Type, [Alsace,Bourgogne,Centre,Limousin,Picardie,
         H1,H2,H3,H4,H5,
         N1,N2,N3,N4,N5,
         F1,F2,F3,F4,F5]).
